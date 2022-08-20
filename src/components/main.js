e6eea3c8816cfbdabf34382716e82d81693725b9
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-08-20 18:59:52 +0300

    test

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+
 
 
 class Main extends React.Component {
